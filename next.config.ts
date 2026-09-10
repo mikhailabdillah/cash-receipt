72d3c6c2c6f04793360b31ee17a1c0ff5d6bd157
mikhail <mikhailabdillahtr@gmail.com>
2026-09-11 03:57:35 +0700

    feat: supabase env set in nextjs config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY,
+  },
 };
 
 export default nextConfig;
